02b0711b95b2a1b6fa391b2992ea8aed862ef9b0
guz86 <guzmo6@rambler.ru>
2021-08-22 12:07:02 +0300

    ver1

--- TwoSum/Program.cs
+++ TwoSum/Program.cs
@@ -4,9 +4,42 @@ namespace TwoSum
 {
     class Program
     {
+
+                // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+                 //You may assume that each input would have exactly one solution, and you may not use the same element twice.
+                  //You can return the answer in any order.
+
+                //Example 1:
+                 //Input: nums = [2, 7, 11, 15], target = 9
+                //Output: [0,1]
+                //Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+                //Example 2:
+                //Input: nums = [3, 2, 4], target = 6
+                //Output: [1,2]
+                //Example 3:
+                //Input: nums = [3, 3], target = 6
+                //Output: [0,1]
+
+        //
+
         static void Main(string[] args)
         {
-            
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+            TwoSum(nums, target);
+        }
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            int[] result = new int[2];
+            for (int i = 0; i < nums.Length-1; i++)
+            {
+                if (nums[i]+ nums[i + 1] == target)
+                {
+                    result[0] = i;
+                    result[1] = i + 1;
+                }
+            }
+            return result;
         }
     }
 }
